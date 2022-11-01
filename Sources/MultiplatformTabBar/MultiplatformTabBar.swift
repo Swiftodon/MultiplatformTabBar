59963408d8fef27a3c090b5b3ea8df1ee5aa47ac
Thomas Bonk <thomas@meandmymac.de>
2022-11-01 11:03:53 +0100

    playing with background colours

--- Sources/MultiplatformTabBar/MultiplatformTabBar.swift
+++ Sources/MultiplatformTabBar/MultiplatformTabBar.swift
@@ -103,7 +103,8 @@ public struct MultiplatformTabBar: View {
                 }
                 ForEach(0..<tabSet.tabs.count) { index in
                     RoundedRectangle(cornerRadius: 10)
-                        .fill(self.selection == index ? Color.systemGray3 : Color.red.opacity(0.0))
+                        //.fill(self.selection == index ? Color.systemGray3 : Color.red.opacity(0.0))
+                        .fill(self.selection == index ? Color.secondaryBackgroundColor : Color.red.opacity(0.0))
                     .overlay(
                         tabSet.tabs[index]
                     )
